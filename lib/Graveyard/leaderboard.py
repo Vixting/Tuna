f75f9c7dfa8d805bcf692a7a399bb872bc103c2b
Vixting <59802277+Vixting@users.noreply.github.com>
2021-02-10 13:04:42 +0000

    commenting out

--- lib/Graveyard/leaderboard.py
+++ lib/Graveyard/leaderboard.py
@@ -2,41 +2,41 @@
 
 
 
-from discord.ext.commands import Cog
-from discord.ext.commands import command 
-from discord import Embed
-import datetime
-import mariadb
-import sys
-from ..db import db
+# from discord.ext.commands import Cog
+# from discord.ext.commands import command 
+# from discord import Embed
+# import datetime
+# import mariadb
+# import sys
+# from ..db import db
 
 
-class server_stats(Cog):
+# class server_stats(Cog):
 
-    def __init__(self, bot):
+#     def __init__(self, bot):
 
-        self.bot = bot
+#         self.bot = bot
 
-    @Cog.listener()
-    async def on_ready(self):
-        if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("leaderboard")    
+#     @Cog.listener()
+#     async def on_ready(self):
+#         if not self.bot.ready:
+#             self.bot.cogs_ready.ready_up("leaderboard")    
     
-    @command(name="leaderboard")
-    async def server_stats(self, ctx, query : str):
-        if (query := query.lower()) in ("kills","aikills","teamkills","capture_points","capture_count","revive_count","bankmoney","bounty","all"):
+#     @command(name="leaderboard")
+#     async def server_stats(self, ctx, query : str):
+#         if (query := query.lower()) in ("kills","aikills","teamkills","capture_points","capture_count","revive_count","bankmoney","bounty","all"):
     
-            conn = mariadb.connect(
-                user="nordic",
-                password="8itEJuK6saKAmE1aL451w326Bujo44",
-                database="a3nordicwasteland",
-                host="178.63.16.206",
-                port=3306)
-            cursor = conn.cursor()
+#             conn = mariadb.connect(
+#                 user="nordic",
+#                 password="8itEJuK6saKAmE1aL451w326Bujo44",
+#                 database="a3nordicwasteland",
+#                 host="178.63.16.206",
+#                 port=3306)
+#             cursor = conn.cursor()
 
         
 
 
 
-def setup(bot):
-    bot.add_cog(server_stats(bot))        
+# def setup(bot):
+#     bot.add_cog(server_stats(bot))        
--- lib/Graveyard/prefix.py
+++ lib/Graveyard/prefix.py
@@ -1,34 +1,34 @@
 
-from discord.ext.commands import Cog
-from discord.ext.commands import CheckFailure
-from discord.ext.commands import command, has_permissions
+# from discord.ext.commands import Cog
+# from discord.ext.commands import CheckFailure
+# from discord.ext.commands import command, has_permissions
 
-from ..db import db
+# from ..db import db
 
-class Misc(Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# class Misc(Cog):
+#     def __init__(self, bot):
+#         self.bot = bot
     
-    @command(name="prefix")
-    @has_permissions(manage_guild=True)
-    async def change_prefix(self, ctx, new: str):
-        if len(new) > 5:
-            await ctx.send("Prefix cannot be longer than 5 characters")
+#     @command(name="prefix")
+#     @has_permissions(manage_guild=True)
+#     async def change_prefix(self, ctx, new: str):
+#         if len(new) > 5:
+#             await ctx.send("Prefix cannot be longer than 5 characters")
         
-        else:
-            db.execute("UPDATE guilds SET Prefix = ? WHERE GuildID = ?",new,ctx.guild.id)
-            await ctx.send(f"Prefix set to {new}")
-            db.commit()
+#         else:
+#             db.execute("UPDATE guilds SET Prefix = ? WHERE GuildID = ?",new,ctx.guild.id)
+#             await ctx.send(f"Prefix set to {new}")
+#             db.commit()
     
-    @change_prefix.error
-    async def change_prefix_error(self, ctx, exc):
-        if isinstance(exc, CheckFailure):
-            await ctx.send("You do not have correct permissions")
+#     @change_prefix.error
+#     async def change_prefix_error(self, ctx, exc):
+#         if isinstance(exc, CheckFailure):
+#             await ctx.send("You do not have correct permissions")
     
-    @Cog.listener()
-    async def on_ready(self):
-        if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("prefix")
+#     @Cog.listener()
+#     async def on_ready(self):
+#         if not self.bot.ready:
+#             self.bot.cogs_ready.ready_up("prefix")
 
-def setup(bot):
-    bot.add_cog(Misc(bot))
+# def setup(bot):
+#     bot.add_cog(Misc(bot))
