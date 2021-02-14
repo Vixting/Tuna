b066fefe3b0469d408cd6bf8350f7e2664ee9946
Vixting <59802277+Vixting@users.noreply.github.com>
2021-02-14 17:15:26 +0000

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -14,13 +14,14 @@ from discord.ext.commands import Context
 from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument,
 							  CommandOnCooldown)
 from discord.ext.commands import when_mentioned_or, command, has_permissions
-
+from os import environ
 from ..db import db
 from discord.ext.commands import when_mentioned_or
 
 OWNER_IDS = [300363459797712906]
 #COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")] #will return python files from cog folder
-COGS = [path.split("/")[-1][:-3] for path in glob("./lib/cogs/*.py")] #will return python files from cog folder
+#COGS = [path.split("/")[-1][:-3] for path in glob("./lib/cogs/*.py")] #will return python files from cog folder
+s3_handler = environ.get("token")
 IGNOR_EXCEPOPTIONS = (CommandNotFound, BadArgument)
 
 
